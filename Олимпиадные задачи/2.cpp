b2155f250e1c708e13af7f44f3423ce5acda2637
LuzinaAnna <luzina.ann03@gmail.com>
2021-10-30 18:47:07 +0500

    'changes'

--- Олимпиадные задачи/2.cpp
+++ Олимпиадные задачи/2.cpp
@@ -1,38 +1,39 @@
 #include <iostream>
 #include <ctime>
+using namespace std;
 
 int main()
 {
 	setlocale(LC_ALL, "Rus");
-	srand(std::time(nullptr));
+	srand(time(nullptr));
 	int x;
 	int a1, i;
 	x = rand() % 100 + 1;
-	std::cout << "Вводите числа от 1 до 100" << std::endl;
-	std::cout << "У вас есть 7 попыток" << std::endl;
+	cout << "Вводите числа от 1 до 100" << endl;
+	cout << "У вас есть 7 попыток" << endl;
 	for (int i = 0; i < 7; i++)
 	{
-		std::cout << i + 1 << "-я попытка :";
-		std::cin >> a1;
+		cout << i + 1 << "-я попытка :";
+		cin >> a1;
 		if (a1 != x)
 		{
 			if(a1 > x)
 			{
-				std::cout << "Нужное число меньше, чем " << a1 << std::endl;
+				cout << "Нужное число меньше, чем " << a1 << endl;
 			}
 			else
 			{
-				std::cout << "Нужное число больше, чем " << a1 << std::endl;
+				cout << "Нужное число больше, чем " << a1 << endl;
 			}
 		}
 		else
 		{
-			std::cout << "Поздравляем, вы отгадали число!" << std::endl;
-			std::exit(0);
+			cout << "Поздравляем, вы отгадали число!" << endl;
+			exit(0);
 		}
 	}
-	std::cout << "Не угадали :(" << std::endl;
-	std::cout << "Число = " << x << std::endl;
+	cout << "Не угадали :(" << endl;
+	cout << "Число = " << x << endl;
 	return 0;
 
 }

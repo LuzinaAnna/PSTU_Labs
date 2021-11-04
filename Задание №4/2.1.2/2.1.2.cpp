1c0aec692375db2f4e7680b64a52166ece17e7d1
LuzinaAnna <luzina.ann03@gmail.com>
2021-11-04 17:27:37 +0500

    task#4

---
+++ Задание №4/2.1.2/2.1.2.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	float i;
+	float p;
+	int k;
+	i = 1 / 7.481;
+	cout << "Введите число галлонов" << endl;
+	cin >> k  ;
+	p = k * i;
+	cout <<"Эквивалентый объем в кубических футах равен "<< p;
+	return 0;
+}
---
+++ Задание №4/2.1/2.1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	int i = 1;
+	cout << ++i * i++ << endl;
+	return 0;
+}
+
